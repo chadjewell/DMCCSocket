c16db3a3c1a22886fb1d69daa27b259b2bdeeba6
chadjewell <chadwickjewell@gmail.com>
2024-01-02 15:58:31 -0600

    Initial commit

---
+++ DMCCSocket/Program.cs
@@ -0,0 +1,68 @@
+﻿using DMCCSocket;
+
+// See https://aka.ms/new-console-template for more information
+
+// Prompt for IP address
+Console.WriteLine("Enter IP Address:");
+string ipAddress = Console.ReadLine();
+//string ipAddress = "10.0.0.147";
+
+// Prompt for Port
+Console.WriteLine("Enter Port #");
+int port = int.Parse(Console.ReadLine());
+//int port = 4545;
+
+// Acknowledge Connect
+Console.WriteLine("Press Enter to Connect");
+Console.ReadKey();
+
+SocketClient dataMan = new SocketClient(ipAddress, port);
+//Console.WriteLine(inSight.Connect().ToString());
+
+// Connect
+Console.WriteLine(dataMan.Connect().ToString());
+List<string> response;
+
+// Prompt for Code 1
+Console.WriteLine("DMCC Command 1");
+//foreach (string line in response)
+//{
+//    Console.WriteLine(line);
+//}
+//dataMan.loggedIn = true;
+
+// Code 1
+string msg = Console.ReadLine();
+response = dataMan.Send(msg);
+foreach (string line in response)
+{
+    Console.WriteLine(line);
+}
+
+// Prompt for Code 2
+Console.WriteLine("DMCC Command 2");
+
+// Code 2
+msg = Console.ReadLine();
+response = dataMan.Send(msg);
+foreach (string line in response)
+{
+    Console.WriteLine(line);
+}
+
+// Prompt for Code 3
+Console.WriteLine("DMCC Command 3");
+
+// Prompt for Command
+msg = Console.ReadLine();
+response = dataMan.Send(msg);
+foreach (string line in response)
+{
+    Console.WriteLine(line);
+}
+
+// Prompt for disconnect
+Console.WriteLine("Press Enter to Disconnect");
+Console.ReadKey();
+
+dataMan.Disconnect();
---
+++ DMCCSocket/SocketClient.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DMCCSocket
+{
+    internal class SocketClient
+    {
+        private IPAddress _address;
+        private IPEndPoint _remoteEP;
+        private Socket _socket;
+        private bool _loggedIn;
+        public bool loggedIn {get { return _loggedIn; } set { _loggedIn = value; } }
+        // Constructor for the socket client, requires string IP address and int port
+        public SocketClient(string ipAddress, int port)
+        {
+            _address = IPAddress.Parse(ipAddress);
+            _remoteEP = new IPEndPoint(_address, port);
+
+            _socket = new Socket(_address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            _socket.ReceiveTimeout = 1000;
+            _loggedIn = false;
+        }
+
+        // Use the connect method to establish a connection with the remote device
+        public bool Connect()
+        {
+            _socket.Connect(_remoteEP);
+
+            return _socket.Connected;
+        }
+        
+        public List<string> Send(string msg)
+        {
+            //_socket.Blocking = false;
+
+            // Send message to the remote device
+            byte[] bMsg = Encoding.ASCII.GetBytes(msg + "\r\n");
+            int bytesSent = _socket.Send(bMsg);
+
+            return this.Receive();
+        }
+
+        // Release the socket
+        public void Disconnect()
+        {
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
+        }
+
+        private List<string> Receive()
+        {
+            // Receive the response from the remote device
+            byte[] buffer = new byte[1024];
+            int bytesRec = _socket.Receive(buffer);
+
+            // Create a list to store the received bytes
+            List<byte> byteList = new List<byte>(buffer.Take(bytesRec));   
+
+            // Convert the list of bytes to a string
+            string response = Encoding.ASCII.GetString(byteList.ToArray());
+
+            // Split the string into an array of messages using the <EOM> delimeter
+            string[] messageArray = response.Split(new string[] { "<EOM>" }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Create a list to store the messages
+            List<string> messages = new List<string>();
+
+            // Add each message to the list
+            messages.AddRange(messageArray);
+
+            // Return the list of received strings
+            return messages;
+        }
+    }
+}
